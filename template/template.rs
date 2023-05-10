6a1daf11c99b736494ef32e628f7ae5f388de7f7
Alexdelia <alexandre.delille.57@gmail.com>
2023-05-10 11:12:17 +0200

    seem to have finished rust

--- template/template.rs
+++ template/template.rs
@@ -1,6 +1,9 @@
+#![allow(dead_code)]
+
 const CONST_YES: &str = r#"yes"#;
 const NO: &str = "no";
 
+#[derive(Debug)]
 pub enum Bar {
     Yes(u8),
     No,
@@ -12,18 +15,18 @@ struct BarStruct<T> {
 }
 
 trait BarTrait {
-	fn yes(&self) -> u8;
-	fn no(&self) -> u8;
+    fn yes(&self) -> u8;
+    fn no(&self) -> u8;
 }
 
 impl BarTrait for BarStruct<u8> {
-	fn yes(&self) -> u8 {
-		self.yes
-	}
+    fn yes(&self) -> u8 {
+        self.yes
+    }
 
-	fn no(&self) -> u8 {
-		self.no
-	}
+    fn no(&self) -> u8 {
+        self.no
+    }
 }
 
 // comment
@@ -33,86 +36,91 @@ multi line comment
 */
 
 /// doc comment
-/// 
+///
 /// # Example
-/// 
+///
 /// ```
 /// let x = 1;
 /// ```
 fn foo(s: impl Into<String>, b: &[u8]) -> Result<Bar, ()> {
     let mut s = s.into();
 
-	let v = vec![1, 2, 3];
-	let x = 0u32;
-	let y: u32 = 0;
-	let c = 'c';
-	let s = "string\n";
-	let s = r#"raw string\n"#;
-	let s = format!("fstring {x} {}", y);
+    let mut v = vec![1, 2, 3];
+    let mut x = 0u32;
+    let y: u32 = 0;
+    let c = 'c';
+    let s = "string\n\x1b[1;35m";
+    let s = r#"raw string\n"#;
+    let s = format!("fstring {x} {}", y);
 
-	x = x + x;
-	x = x - x;
-	x = x * x;
-	x = x / x;
-	x += x;
-	x -= x;
-	x *= x;
-	x /= x;
-	x += 1;
-	x -= 1;
+    let d = 10;
+    let b = 0b10;
+    let o = 0o10;
+    let h = 0x10;
 
-	v[0] = 42;
-	v.len();
-	v[0..1];
+    x = x + x;
+    x = x - x;
+    x = x * x;
+    x = x / x;
+    x = x % x;
+    x += x;
+    x -= x;
+    x *= x;
+    x /= x;
+    x %= x;
+    x += 1;
+    x -= 1;
 
-	let raw_ptr = &x as *const u32;
-	let addr = raw_ptr as usize;
+    v[0] = 42;
+    v.len();
 
-	x = if x == y {
-		1
-	} else if x != y {
-		2
-	} else {
-		3
-	};
+    let raw_ptr = &x as *const u32;
+    let addr = raw_ptr as usize;
 
-	x = match x {
-		1 => 1,
-		2 => 2,
-		_ => 3,
-	};
+    x = if x == y {
+        1
+    } else if x != y {
+        2
+    } else {
+        3
+    };
 
-	if let Some(x) = x {
-		println!("{x}");
-	} else {
-		println!("no x");
-	};
+    x = match x {
+        1 => 1,
+        2 => 2,
+        _ => 3,
+    };
 
-	while x < 10 {
-		x += 1;
-	}
+    if let Some(x) = Some(1) {
+        println!("{x}");
+    } else {
+        println!("no x");
+    };
 
-	for x in 0..10 {
-		let d = Some(x);
-		println!("{d:?}");
-	}
+    while x < 10 {
+        x += 1;
+    }
 
-	loop {
-		println!("loop");
-		break;
-	}
+    for x in 0..=42 {
+        let d = Some(x);
+        println!("{d:?}");
+    }
 
-	let b: bool = true;
-	let b = false;
+    loop {
+        println!("loop");
+        break;
+    }
 
-	if ((x == x || x != x) && x == x && sizeof(x))
-	while (((((true)))))
-		x = false;
+    let b: bool = true;
+    let b = false;
 
-	foo(x, NULL, NULL);
+    foo("str", &[1, 2, 3]);
 
-	)
-	// comment
+    let bar = BarStruct::<u8> { yes: 1, no: 2 };
 
-    s.parse()
+    if bar.yes() == 1 {
+        Ok(Bar::Yes(1))
+    } else {
+        Err(())
+    }
 }
